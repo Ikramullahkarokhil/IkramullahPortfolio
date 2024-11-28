ad0a17f0e69f3bb0abeb2649943ac32a6a468c8f
Ikramullahkarokhil <ikramullahkarokhil@gmail.com>
2024-11-28 01:49:08 -0800

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
-import compress from 'vite-plugin-compression'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), compress(), VitePWA({
-    registerType: 'autoUpdate',
-    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    manifest: {
-      name: 'Ikramullah Karokhail Portfolio',
-      short_name: 'IK Portfolio',
-      theme_color: '#2563eb',
-      icons: [
-        {
-          src: '/logo.svg',
-          sizes: '192x192',
-          type: 'image/svg+xml'
-        }
-      ]
-    }
-  })],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
